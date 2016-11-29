f37f0703fd886ef66fe126241442e45ebd691db4
ranjitrn5 <ranjitrn5@gmail.com>
2016-11-28 23:04:38 -0600

    Commit after adding UI files

---
+++ das-boot/src/main/java/com/boot/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.boot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home(){
+		return "Das Boot, reporting for duty";
+	}
+}
